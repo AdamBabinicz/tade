95b3fdf18d3eb3a4a2fa1db6296234f7b3d07c51
babinicz <babinicz@onet.pl>
2021-08-21 20:21:21 +0200

    dodałem popup

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Favorite.css";
 import favorite from "../images/4.jpg";
 import Slide from "react-reveal/Reveal";
+import Popup from "./Popup";
+import Img1 from "../images/9.svg";
 
 const Favorite = () => {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  // const [buttonPopup1, setButtonPopup1] = useState(false);
+  const [state] = useState({
+    title: "Ile kalorii spalamy na rowerze?",
+    img: Img1,
+    p0: "Przyjmuje się, że jazda na rowerze ze średnią prędkością ok. 20 km/h przez godzinę pozwala spalić 400 – 500 kcal. W ciągu pół godzinnej przejażdżki jesteśmy w stanie pozbyć się ok. 200 – 300 kcal.",
+    em: "pgbikes.pl/porady-rowerowe/porady",
+  });
+
   return (
     <div className="favContainer" id="favorite">
       <h2>Moje ulubione zajęcie</h2>
@@ -22,7 +33,17 @@ const Favorite = () => {
           </div>
         </Slide>
       </div>
-      <button className="favBtn">Zobacz</button>
+      <button className="favBtn" onClick={() => setButtonPopup(true)}>
+        Zobacz
+      </button>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3 style={{ fontSize: "15px" }}>{state.title}</h3>
+        <br />
+        <img src={state.img} alt="..." /> <br />
+        <p style={{ fontSize: "14px", textAlign: "left" }}>{state.p0}</p>
+        <em style={{ fontSize: "11px" }}>{state.em}</em>
+        <br />
+      </Popup>
     </div>
   );
 };
